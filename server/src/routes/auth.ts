c764cb6aa053232bd508aae8318452d56ebc2354
Avashneupane9857 <juisch20220011252@gmail.com>
2024-12-07 22:28:44 +0530

    feat: auth server

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,3 +1,44 @@
-export const authRoutes=(req:any,res:any)=>{
+import { Router } from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { prisma } from "../prisma";
 
-}
+const JWT_SECRET = process.env.JWT_SECRET as string;
+
+export const authRoutes = Router();
+
+authRoutes.post("/signup", async (req, res) => {
+    const { username, password, role } = req.body;
+  
+    try {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const user = await prisma.user.create({
+        data: { username, password: hashedPassword, role },
+      });
+      res.status(201).json({ message: "User created", user });
+    } catch (error) {
+      res.status(400).json({ error: "User already exists" });
+    }
+  });
+
+  authRoutes.post("/signin", async (req:any, res:any) => {
+    const { username, password } = req.body;
+  
+    try {
+      const user = await prisma.user.findUnique({ where: { username } });
+      if (!user || !(await bcrypt.compare(password, user.password))) {
+        return res.status(401).json({ error: "Invalid credentials" });
+      }
+  
+      const token = jwt.sign({ userId: user.id, role: user.role }, JWT_SECRET, {
+        expiresIn: "1h",
+      });
+      res.status(200).json({ token, role: user.role });
+    } catch (error) {
+      res.status(500).json({ error: "Something went wrong" });
+    }
+  });
+  
+  export default authRoutes;
+
+
