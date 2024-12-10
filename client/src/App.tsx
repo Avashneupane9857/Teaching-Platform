7afd14e074289234c3242b78829ec1e812545acb
Avashneupane9857 <juisch20220011252@gmail.com>
2024-12-10 20:13:32 +0530

    extra: fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Auth from "./pages/Auth";
+
 import Dashboard from "./pages/Dashboard";
 import Classroom from "./pages/Classroom";
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Auth />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/classroom/:classId" element={<Classroom />} />
       </Routes>
