ab3120bcedc60120b3c204ba55a958a08410117a
Avashneupane9857 <juisch20220011252@gmail.com>
2024-12-12 22:33:21 +0530

    feat: ws server fixed

--- server/dist/websocket.js
+++ server/dist/websocket.js
@@ -4,13 +4,13 @@ const ws_1 = require("ws");
 const clients = {};
 const wss = new ws_1.WebSocketServer({ port: 8080 });
 wss.on("connection", (socket) => {
+    console.log('New client connected');
     socket.on("message", (message) => {
         const parsed = JSON.parse(message.toString());
         const { type, classId, content } = parsed;
         if (!clients[classId])
             clients[classId] = new Set();
         clients[classId].add(socket);
-        // Broadcast to all clients in the class
         clients[classId].forEach((client) => {
             if (client !== socket) {
                 client.send(JSON.stringify({ type, content }));
